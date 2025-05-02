050b0d1742d134075698359b89d581afe29f00f9
HP <mintesinotbekele1240@gmail.com>
2025-05-02 18:56:44 +0300

    Make build.sh executable

--- build.sh
+++ build.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 mix deps.get
 MIX_ENV=prod mix compile
-# npm --prefix assets install
-# npm run --prefix assets deploy
-MIX_ENV=prod mix phx.digest
+MIX_ENV=prod mix assets.deploy
+MIX_ENV=prod mix release
