537853df960dd8ed7cadd0d34a2ee9a662699a54
HP <mintesinotbekele1240@gmail.com>
2025-05-02 14:55:58 +0300

    Fix build.sh for Render

--- build.sh
+++ build.sh
@@ -1,14 +1,20 @@
 #!/usr/bin/env bash
 set -e
 
+# Install Hex and Rebar (build tools for Elixir dependencies)
 mix local.hex --force
 mix local.rebar --force
 
+# Get and compile dependencies in prod mode
 mix deps.get --only prod
 MIX_ENV=prod mix deps.compile
 
+# Build JS/CSS assets
 npm install --prefix assets
 npm run deploy --prefix assets
+
+# Digest static assets
 mix phx.digest
 
+# Build the release
 MIX_ENV=prod mix release
