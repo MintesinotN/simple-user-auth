7b0a10f79bdca8ba6e404490eddc94504227bbde
HP <mintesinotbekele1240@gmail.com>
2025-05-02 18:26:56 +0300

    unnecessary command removal

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 mix deps.get
 MIX_ENV=prod mix compile
-npm --prefix assets install
-npm run --prefix assets deploy
+# npm --prefix assets install
+# npm run --prefix assets deploy
 MIX_ENV=prod mix phx.digest
